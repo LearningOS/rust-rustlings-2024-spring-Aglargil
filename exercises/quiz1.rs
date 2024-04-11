9e3fc991942c8ee9a0debeb905e014ea1d22e338
Aglargil <34728006+Aglargil@users.noreply.github.com>
2024-04-11 17:35:00 +0000

    update

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -13,10 +13,11 @@
 //
 // No hints this time ;)
 
-// I AM NOT DONE
-
 // Put your function here!
-// fn calculate_price_of_apples {
+fn calculate_price_of_apples(num: i32) -> i32 {
+    let factor = if num > 40 { 1 } else { 2 };
+    factor * num
+}
 
 // Don't modify this function!
 #[test]
